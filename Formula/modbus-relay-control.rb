c65a6105574f4dc02d1f9b3c45256be314d83700
Will Curtis <will@willsplace.co.uk>
2025-04-27 20:07:59 +0100

    Update modbus-relay-control.rb

--- Formula/modbus-relay-control.rb
+++ Formula/modbus-relay-control.rb
@@ -1,7 +1,7 @@
 class ModbusRelayControl < Formula
   desc "Modbus TCP Relay Board Controller CLI"
   homepage "https://github.com/willcurtis/modbus-relay-control"
-  url "https://github.com/willcurtis/modbus-relay-control/archive/refs/tags/v1.0.1.tar.gz"
+  url "https://github.com/willcurtis/modbus-relay-control/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
 
